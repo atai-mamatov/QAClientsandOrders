edd06e6035de2b52124f30034e0522855b503c24
atai-mamatov <altair.mamatov@gmail.com>
2023-02-28 19:56:14 +0600

    Added Choice of Creation of Client

--- ClientsandOrders2/Menu.cs
+++ ClientsandOrders2/Menu.cs
@@ -41,44 +41,6 @@ namespace ClientsandOrders2.CMD
 
 
 
-            //Console.WriteLine("Choose an action:");
-            //Console.WriteLine("1. Add client");
-            //Console.WriteLine("2. Edit client");
-            //Console.WriteLine("3. Remove client");
-            //Console.WriteLine("4. Add order");
-            //Console.WriteLine("5. Edit order");
-            //Console.WriteLine("6. Remove order");
-            //Console.WriteLine("0. Exit");
-
-            //int choice = Convert.ToInt32(Console.ReadLine());
-
-            //switch (choice)
-            //{
-            //    //case 1:
-            //    //    ClientManager.MakeClient();
-            //    //    Console.WriteLine("Client added successfully.");
-            //    //    break;
-            //    //case 2:
-            //    //    Console.WriteLine("Enter client index to edit:");
-            //    //    int index = Convert.ToInt32(Console.ReadLine());
-            //    //    Console.WriteLine("Enter first name:");
-            //    //    firstName = Console.ReadLine();
-            //    //    Console.WriteLine("Enter second name:");
-            //    //    secondName = Console.ReadLine();
-            //    //    Console.WriteLine("Enter phone number:");
-            //    //    phoneNum = Console.ReadLine();
-            //    //    Console.WriteLine("Enter order amount:");
-            //    //    OrderAmount = Convert.ToUInt32(Console.ReadLine());
-            //    //    Console.WriteLine("Enter date added (yyyy-MM-dd):");
-            //    //    dateAdd = DateTime.Parse(Console.ReadLine());
-            //    //    ClientManager.EditClient(index, firstName, secondName, phoneNum);
-            //    //    Console.WriteLine("Client edited successfully.");
-            //    //    break;
-
-
-
-
-            //}
         }
 
         public static void Next<T>() where T : BaseEntity
@@ -96,6 +58,29 @@ namespace ClientsandOrders2.CMD
                 }
                 Console.WriteLine(table);
                 Console.ReadKey();
+                Console.WriteLine("Choose what you would like to do\n" +
+                    "Add Client Press A - [A]\n" +
+                    "Edit Client Press E - [E]\n" +
+                    "Delete Client Press D - [D]");
+
+                var choice = Console.ReadKey(true);
+
+                while (choice.Key != ConsoleKey.A && choice.Key != ConsoleKey.E && choice.Key != ConsoleKey.D)
+                {
+                    choice = Console.ReadKey(true);
+                }
+
+                switch (choice.Key)
+                {
+                    case ConsoleKey.A:
+                        ClientManager.AddClient();
+                        break;
+                    case ConsoleKey.E:
+                        Next<Order>();
+                        break;
+
+                }
+
             }
             else if (typeof(T) == typeof(Order))
             {
