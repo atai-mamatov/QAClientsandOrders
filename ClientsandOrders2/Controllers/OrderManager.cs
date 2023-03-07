a65dba1d30987b7eca6441768505afbde04a8310
atai-mamatov <altair.mamatov@gmail.com>
2023-03-07 19:05:22 +0600

    New stuff

--- ClientsandOrders2/Controllers/OrderManager.cs
+++ ClientsandOrders2/Controllers/OrderManager.cs
@@ -1,49 +1,52 @@
 ﻿using ClientsandOrders.Data.Models.Entities;
+using ClientsandOrders2.CMD;
 
 namespace ClientsandOrders.BL.Controllers
 {
     public class OrderManager
     {
         private List<Order> clients = new List<Order>();
-        public void Add(string description, float orderPrice, DateTime orderDate, uint clientId)
+        public Order MakeOrder(string description, float orderPrice, DateTime orderDate, uint clientId)
         {
-            Order order = new Order
+            Console.Clear();
+            Console.WriteLine("Lets create an order!\n");
+            List<Client> clients = Menu.clientController.GetAll();
+            if(clients.Count == 0)
+            {
+                Console.WriteLine("There are no clients yet. Please create a client\n" +
+                    "Press any key to continue");
+                Console.ReadKey();
+                Menu.Start();
+            }
+            Console.WriteLine("Choose which client you want add order to please");
+            for(int i = 0; i < clients.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {clients[i]}");
+            }
+            Console.WriteLine("Enter number of your client please");
+            int clientNumber = int.Parse(Console.ReadLine());
+            Client client = clients[clientNumber - 1];
+            string description = ConsoleHelper.GetString("description");
+            float orderPrice = ConsoleHelper.GetInt("order price");
+            DateTime dateAdd = DateTime.Now;
+            Order order = new Order()
             {
                 Description = description,
                 OrderPrice = orderPrice,
-                OrderDate = orderDate,
-                ClientId = clientId,
+                OrderDate = dateAdd,
             };
+            return order;
         }
 
         public void Edit(uint orderId, string description, float orderPrice, DateTime orderDate, DateTime closeDateTime)
         {
-            Order order = clients.FirstOrDefault(o => o.Id == orderId);
-            if (order == null)
-            {
-                throw new ArgumentException($"Order with id={orderId} not found.");
-            }
 
-            order.Description = description;
-            order.OrderPrice = orderPrice;
-            order.OrderDate = orderDate;
-            order.CloseDate = closeDateTime;
-
-           
         }
 
 
         public void Delete(uint orderId)
         {
-            Order order = clients.FirstOrDefault(o => o.Id == orderId);
-            if (order != null)
-            {
-                clients.Remove(order);
-            }
-            else
-            {
-                throw new ArgumentException($"Order with id={orderId} not found.");
-            }
+
         }
 
 
